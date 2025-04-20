4accbc94ab3b6dca87ce857a5b5b7ed9ed8169bb
Nakidai <nakidai@disroot.org>
2025-04-20 23:04:54 +0300

    Simplyify code
    Now it's smaller than the docs lol

--- deansi.c
+++ deansi.c
@@ -1,22 +1,10 @@
 #include <stdio.h>
-
-
 int main(int argc, char **argv)
 {
-	int ch, next, csi;
-
-	csi = 0;
+	int ch, next, csi = 0;
 	while ((ch = getchar()) != EOF)
-	{
-		if (ch == 27 /* ^[ */)
-			if ((next = getchar()) == '[')
-				csi = 1;
-			else if (next != EOF)
-				ungetc(next, stdin);
-		if (!csi)
-			putchar(ch);
-		if (ch >= 0x40 && ch <= 0x7e)
-			csi = 0;
-	}
+		ch == 0x1b && ((next = getchar()) == '[' ? csi = 1 : ungetc(next, stdin)),
+		!csi && putchar(ch),
+		ch >= 0x40 && ch <= 0x7e && (csi = 0);
 	return ferror(stdin);
 }
